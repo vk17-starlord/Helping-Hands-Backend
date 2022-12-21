41466d711c765bdf5e6f34be0630baab4e5f4cc4
ishan-18 <ishangawali02@gmail.com>
2022-12-21 16:57:43 +0530

    Queries added

--- routes/job.js
+++ routes/job.js
@@ -8,11 +8,63 @@ const User = require('../models/User');
 
 router.get('/', async (req,res) => {
     try {
-        const job = await Job.find();
+
+        let query;
+
+        console.log(req.params)
+
+        const reqQuery = {...req.query} 
+
+        const removeFields = ['select','sort', 'page', 'limit'];
+        removeFields.forEach(param => delete reqQuery[param]);
+
+
+        let queryStr = JSON.stringify(reqQuery);
+
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+        query = Job.find(JSON.parse(queryStr))
+
+        if(req.query.select){
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields)
+        }
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        }else{
+            query = query.sort('-createdAt');
+        }
+
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit
+        const total = await Job.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
+        const pagination = {};
+        if(endIndex < total){
+            pagination.next = {
+                page: page + 1,
+                limit
+            };
+        }
+
+        if(startIndex > 0){
+            pagination.prev = {
+                page: page - 1,
+                limit
+            };
+        }
+
+        const job = await query;
         if(job){
             res.status(200).json({
-                success: true,
+                status: true,
                 count: job.length,
+                pagination,
                 data: job
             })
         }
